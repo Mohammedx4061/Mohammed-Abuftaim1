9949c91eda80c9e296ff2ebd6467fc52e6df44da
Mohammedx4061 <mohammedx406@gmail.com>
2025-11-26 20:01:01 +0300

    text Fild

--- lib/main.dart
+++ lib/main.dart
@@ -1,146 +1,181 @@
-
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(Profile());
+  runApp(const MyApp());
 }
 
-class Profile extends StatelessWidget {
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        backgroundColor: Colors.blue[50],
-        appBar: AppBar(
-          title: Text('تكليف الفلاتر', style: TextStyle(color: Colors.black),),
-        ),
-        body: Column(
+      title: 'Textfield Task',
+
+      // تحديد الصفحة الرئيسية
+      home: const TextFieldScreen(),
+    );
+  }
+}
+
+class TextFieldScreen extends StatefulWidget {
+  const TextFieldScreen({super.key});
+
+  @override
+  State<TextFieldScreen> createState() => _TextFieldScreenState();
+}
+
+class _TextFieldScreenState extends State<TextFieldScreen> {
+
+  final TextEditingController _controller1 = TextEditingController();
+  final TextEditingController _controller2 = TextEditingController();
+
+
+  void _copyText() {
+    setState(() {
+      _controller2.text = _controller1.text;
+    });
+  }
+
+
+  void _navigateToNextPage() {
+    String dataToSend = _controller1.text;
+
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+
+        builder: (context) => TwoPage(passedName: dataToSend),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+
+    _controller1.dispose();
+    _controller2.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Page'),
+        backgroundColor: Colors.blue,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
           children: [
-            Container(
-              width: double.infinity,
-              color: Color(0xFF2226D1),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  SizedBox(height: 15),
-                  CircleAvatar(
-                    radius: 80,
-                    backgroundColor: Colors.white,
-                    child: CircleAvatar(
-                      radius: 80,
-                      backgroundImage: Image.asset(
-                        'image/imageflatter.png',
-                      ).image,
-                    ),
-                  ),
-                  SizedBox(height: 20),
-                  Text(
-                    'Mohammed Abuftaim',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 20,
-                    ),
-                  ),
-                  Divider(color: Colors.black),
-                  Text(
-                    'IT Student',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 20,
-                    ),
-                  ),
-                  SizedBox(height: 17),
-                ],
+            const SizedBox(height: 20),
+
+
+            TextField(
+              controller: _controller1,
+              decoration: InputDecoration(
+                labelText: 'Enter the text here',
+                hintText: 'For Example: mohammed',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(50.0),
+                ),
               ),
             ),
-            Container(
-              alignment: Alignment.center,
-              width: double.infinity,
-              height: 40,
-              color: Color(0xFF2B46FF),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  Text('Email',
-                      style: TextStyle(fontSize: 20, color: Colors.white)),
-                  Text('muhammed.04@email.com',
-                      style: TextStyle(fontSize: 20, color: Colors.white))
-                ],
+            const SizedBox(height: 40),
+
+
+            TextField(
+              controller: _controller2,
+              readOnly: true,
+              decoration: InputDecoration(
+                labelText: 'Printed Text',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(50.0),
+                ),
               ),
             ),
-            Container(
-              alignment: Alignment.center,
-              width: double.infinity,
-              height: 40,
-              color: Color(0xFF0E255A),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  Text('Phone :',
-                      style: TextStyle(fontSize: 20, color: Colors.white)),
-                  Text('738154451',
-                      style: TextStyle(fontSize: 20, color: Colors.white))
-                ],
+            const SizedBox(height: 40),
+
+
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.blue,
+                minimumSize: const Size(double.infinity, 50),
               ),
+              onPressed: _copyText,
+              child: const Text('Submit'),
             ),
-            Container(
-              width: double.infinity,
-              height: 220,
-              color: Color(0xFF031D9E),
-              child: Column(
-                children: [
-                  Text('Skils :',
-                      style: TextStyle(fontSize: 24, color: Colors.white)),
-                  Row(
-                    children: [
-                      Spacer(),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text('Scientific :',
-                              style:
-                              TextStyle(fontSize: 20, color: Colors.white)),
-                          SizedBox(height: 12),
-                          Text('1- Software Design ',
-                              style: TextStyle(
-                                  fontSize: 15, color: Colors.white)),
-                          Text('2- Database Design ',
-                              style: TextStyle(
-                                  fontSize: 15, color: Colors.white)),
-                          Text('3- Problem Solving ',
-                              style: TextStyle(
-                                  fontSize: 15, color: Colors.white)),
-                        ],
-                      ),
-                      Spacer(),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text('Practicl :',
-                              style:
-                              TextStyle(fontSize: 20, color: Colors.white)),
-                          SizedBox(height: 12),
-                          Text('1- Firebase / Supabase ',
-                              style: TextStyle(
-                                  fontSize: 15, color: Colors.white)),
-                          Text('2- Responsive Design ',
-                              style: TextStyle(
-                                  fontSize: 15, color: Colors.white)),
-                          Text('3- Deployment (Play Store / App Store) ',
-                              style: TextStyle(
-                                  fontSize: 15, color: Colors.white)),
-                        ],
-                      ),
-                      Spacer()
-                    ],
-                  )
-                ],
+            const SizedBox(height: 20),
+
+
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.blue,
+                minimumSize: const Size(double.infinity, 50),
               ),
+              onPressed: _navigateToNextPage,
+              child: const Text('Navigate'),
             ),
           ],
         ),
       ),
     );
   }
+}
+
+// 2. الصفحة الثانية
+
+class TwoPage extends StatelessWidget {
+
+  final String passedName;
+
+
+  const TwoPage({
+    super.key,
+    required this.passedName,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Received Data Page'),
+          backgroundColor: Colors.blue,
+        ),
+        body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  'Data Successfully received:',
+                  style: TextStyle(fontSize: 25),
+                ),
+                const SizedBox(height: 20),
+
+                Text(
+                  passedName,
+                  style: const TextStyle(
+                    fontSize: 38,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.green,
+                  ),
+                ),
+                const SizedBox(height: 50),
+                ElevatedButton(
+                  onPressed: () {
+
+                    Navigator.pop(context);
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blue,
+                    minimumSize: const Size(double.infinity, 60),
+                  ),
+                  child: const Text('Back'),
+                ),
+              ],
+            ),
+            ),
+        );
+    }
 }
