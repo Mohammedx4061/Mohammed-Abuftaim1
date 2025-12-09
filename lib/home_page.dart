7912d043db1bc37c827e090f4acec29fa90f268b
Mohammedx4061 <mohammedx406@gmail.com>
2025-12-09 08:41:45 +0300

    Project

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -5,14 +5,65 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Game Store", style: TextStyle(fontSize: 26, color: Colors.white)),
+        title: Center(
+          child: Text(
+            "Game Store",
+            style: TextStyle(
+              fontSize: 26,
+              color: Colors.white,
+              fontWeight: FontWeight.w800,
+            ),
+          ),
+        ),
+      ),
+
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.black,
+              ),
+              child: Column(
+                children: [
+                  Text(
+                    "Search Games",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 15),
+                  TextField(
+                    decoration: InputDecoration(
+                      hintText: "Type game name...",
+                      hintStyle: TextStyle(color: Colors.white70),
+                      filled: true,
+                      fillColor: Colors.white24,
+                      prefixIcon: Icon(Icons.search, color: Colors.white),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide.none,
+                      ),
+                    ),
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
 
       body: SingleChildScrollView(
         child: Column(
           children: [
             SizedBox(height: 20),
-            Text("Top Games", style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
+            Text(
+              "Top Games",
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+            ),
             SizedBox(height: 20),
 
             Container(
@@ -43,10 +94,16 @@ class HomePage extends StatelessWidget {
       ),
       child: Column(
         children: [
-          Image.asset(imagePath, height: 180, fit: BoxFit.cover),
+          Image.asset(
+            imagePath,
+            height: 180,
+            fit: BoxFit.cover,
+          ),
           SizedBox(height: 10),
-          Text(title, style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
-
+          Text(
+            title,
+            style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+          ),
         ],
       ),
     );
